d8bab971ecf381691fcef62e2b9369e25a7e7d9d
Motty Cohen <motty.cohen@gmail.com>
2023-06-21 11:06:33 +0300

    Update ConcurrentStringMap

--- utils/collections/concurrent_string_map.go
+++ utils/collections/concurrent_string_map.go
@@ -4,31 +4,26 @@ import (
 	"sync"
 )
 
-type ConcurrentStringMap struct {
-	sync.Mutex
-	m map[string]string
+type ConcurrentStringMap[T any] struct {
+	sync.RWMutex
+	m map[string]T
 }
 
-func (c *ConcurrentStringMap) Get(key string) (val string) {
-	var ok bool
-
-	c.Lock()
-	defer c.Unlock()
-
-	if val, ok = c.m[key]; ok {
-	}
-
-	return
+func (c *ConcurrentStringMap[T]) Get(key string) (T, bool) {
+	c.RLock()
+	val, found := c.m[key]
+	c.RUnlock()
+	return val, found
 }
 
-func (c *ConcurrentStringMap) Put(key string, val string) {
+func (c *ConcurrentStringMap[T]) Put(key string, val T) {
 	c.Lock()
-	defer c.Unlock()
 	c.m[key] = val
+	c.Unlock()
 }
 
-func NewConcurrentStringMap() ConcurrentStringMap {
-	return ConcurrentStringMap{
-		m: make(map[string]string),
+func NewConcurrentStringMap[T any]() ConcurrentStringMap[T] {
+	return ConcurrentStringMap[T]{
+		m: make(map[string]T),
 	}
 }
